3b215c98afc5eeef8b60484c7e2c4f5353b2279f
sshimek42 <92750108+sshimek42@users.noreply.github.com>
2022-08-05 11:18:48 -0500

    Updates

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 setup(
-    name="sclogging",
-    version="0.0.40",
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    package_data={"sclogging": ["config.py", "settings.toml"]},
-    url="",
-    license="",
-    author="",
-    author_email="",
-    description="",
-)
+    name='sclogging',
+    version='0.0.40',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    package_data={'sclogging': ['config.py', 'settings.toml']},
+    url='https://github.com/sshimek42/sclogging',
+    license='MIT',
+    author='',
+    author_email='',
+    description=''
+    )
