982745cd8b595c381f802c9f32dcf7dcc7880792
shauns <shaun.shimek@shimekit.com>
2023-01-03 11:15:59 -0600

    Fixed color format

--- src/sclogging/sclogging_main.py
+++ src/sclogging/sclogging_main.py
@@ -84,7 +84,7 @@ class NameFilter(logging.Filter):
         fixed_text = record.msg
         curr_bold = ""
         curr_faint = ""
-        style_tag = {"f": "cr.Fore", "s": "cr.Style", "b": "back"}
+        style_tag = {"f": "fore", "s": "style", "b": "back"}
         each_attrib_count = 0
         for style_search in style_tag:
             style_regex = r"%" + style_search + \
