5860ce0ff476c2081aa7c080aa3ef9ccf446006f
shauns <shaun.shimek@shimekit.com>
2023-09-13 08:06:01 -0500

    Fixed spacer issue

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sclogging",
+    version="1.0.2",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    package_data={"sclogging": ["config.py", "settings.toml"]},
+    url="https://github.com/sshimek42/sclogging",
+    license="MIT",
+    author="",
+    author_email="",
+    description="",
+)
